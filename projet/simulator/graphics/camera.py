d27bd38fec6a203d63f25efaf9ef1ee486c228e5
Sarah Abbana <ninie@MacBook-Air-de-Sarah.local>
2021-05-11 17:44:07 +0200

    test

--- projet/simulator/graphics/camera.py
+++ projet/simulator/graphics/camera.py
@@ -1,15 +1,16 @@
 from ..utils.vector import Vector2
 
 
-class Camera:
+class Camera:       #donne une position et une échelle; rectangle sur un plan en gros 
     def __init__(self, screen_size):
-        self.screen_size = screen_size
+        self.screen_size = screen_size  #vecteur de taille 2
         self.position = Vector2(0, 0)
-        self.scale = 1
+        self.scale = 1      #echelle du monde par rapport à l'écran 
 
     def to_screen_coords(self, position):
         """ Converts the world-coordinate position to a screen-coordinate. """
-        raise NotImplementedError
+        positionreduite = position * self.scale
+        
 
     def from_screen_coords(self, position):
         """ Converts the screen-coordinate position to a world-coordinate. """
